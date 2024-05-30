61b22a4a4fce827d6199aa56010c1eb05a9bff44
wangheng <2050641814@qq.com>
2024-05-30 16:04:18 +0800

    表格驱动测试

--- struct-methods-interface/main_test.go
+++ struct-methods-interface/main_test.go
@@ -1,13 +1,37 @@
 package struct_methods_interface
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
-func Perimeter(width float64, heigth float64) float64 {
-	return 2 * (width + heigth)
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{
+		Width:  10.0,
+		Height: 10.0,
+	}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -15,15 +39,51 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+type Shape interface {
+	Area() float64
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{
+			Width:  12.0,
+			Height: 6.0,
+		}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{Radius: 10}
+		checkArea(t, circle, 314.1592653589793)
+	})
+}
+
+func TestArea1(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
 	}
 }
