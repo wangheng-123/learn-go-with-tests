c811e34de371c1e38ba4bc375e4e1c4b5606f1ab
wangheng <2050641814@qq.com>
2024-05-30 13:07:36 +0800

    最后一次重构

--- hello-world/main.go
+++ hello-world/main.go
@@ -12,14 +12,19 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
 	case french:
 		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return
 }
 
 func main() {
