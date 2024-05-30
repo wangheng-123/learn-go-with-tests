f157a90fa019f22e4d2a4dd8a7204632457ac259
wangheng <2050641814@qq.com>
2024-05-30 12:53:23 +0800

    重构测试

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -9,3 +9,44 @@ func TestHello(t *testing.T) {
 		t.Errorf("got '%q' want '%q'", got, want)
 	}
 }
+
+func TestHello2(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris"
+
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+}
+
+func TestHello3(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+		assertCorrectMessage(t, got, want)
+	})
+}
