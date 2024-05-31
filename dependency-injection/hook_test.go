061ec4526d0e9f00936ef661ba6baf0fa9b1952f
wangheng <2050641814@qq.com>
2024-05-31 13:16:56 +0800

    依赖注入测试

---
+++ dependency-injection/hook_test.go
@@ -0,0 +1,23 @@
+package dependency_injection
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func Greet(writer *bytes.Buffer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
